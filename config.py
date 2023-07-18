71befcea4971ba4f828752b0e42c01274f41986f
crazyworld-izzy <126413936+crazyworld-izzy@users.noreply.github.com>
2023-07-19 00:31:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "13399054"))
+API_HASH = getenv("API_HASH", "585801d590dac4c79aeaa7bcda495e62")
 
-TOKEN = getenv("TOKEN")
-MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+TOKEN = getenv("TOKEN", "6009759960:AAEMrabQKJcYXvw3QwNLDHryQ0IGniDYkJU")
+MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://moni1:ammu@cluster0.lmgyalw.mongodb.net/?retryWrites=true&w=majority")
 
-OWNER_ID = int(getenv("OWNER_ID", 5938660179))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", "6083192834"))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tamil_chating_junctions")
 
-LOGGER_ID = getenv("LOGGER_ID", None)
+LOGGER_ID = getenv("LOGGER_ID", "-1001932337367")
